425a05b3a1df939276be9c6072720d453d03748f
Samrith Yoeun <samrith.yoe@ascendcorp.com>
2019-12-06 09:13:10 +0700

    first init

---
+++ ios-http-sdk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-http-sdk
+//
+//  Created by Yoeun Samrith on 12/6/19.
+//  Copyright © 2019 Yoeun Samrith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
